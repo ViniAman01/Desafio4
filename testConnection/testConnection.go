e4fd2393ce2fdaa60292da6cdbfbefe2898a7b79
ViniAman01 <vinisaman@gmail.com>
2023-08-29 11:20:24 -0400

    Relizados testes utilzando dbConnection

--- testConnection/testConnection.go
+++ testConnection/testConnection.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"project/dbConnection"
 	"project/models"
 )
 
+var media = models.Media{
+	Data_type: "tipoA",
+	Code_book: "12345",
+	Description_page: "cover_page",
+	Data: "informações",
+}
+
+var media2 = models.Media{
+	Data_type: "tipoB",
+	Code_book: "12345",
+	Description_page: "page2",
+	Data: "informações",
+}
+
+var media3 = models.Media{
+	Data_type: "tipoC",
+	Code_book: "12345",
+	Description_page: "page3",
+	Data: "informações",
+}
+
+var media4 = models.Media{
+	Data_type: "tipoD",
+	Code_book: "12345",
+	Description_page: "page4",
+	Data: "informações",
+}
+
+var medias = []models.Media{media,media2,media3,media4}
 
 func main(){
 	client := dbConnection.ConnectDB()
 
-	db := dbConnection.GetDB("dbtest", client)
+	coll := dbConnection.GetCollection("mainDB2","collteste",client)
 
-	fmt.Println(db)
-
-	coll := dbConnection.GetCollection("dbtest","collteste",client)
-
-	fmt.Println(coll)
-
-	dbConnection.InsertColl(coll,models.Media{Data_type: "teste1",Code_book: "teste1",Description_page: "teste1", Data: "teste1"})
+	for _,media := range medias {
+		dbConnection.InsertColl(coll,media)
+	}
 }
